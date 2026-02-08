3c12cf668f4a91808d944ef973b3a9a020b50941
Juan Antonio Buendia Sánchez <jabuendias@eafit.edu.co>
2026-02-07 20:28:52 -0500

    Visualización de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
     # Render the home.html template
     return render(request, 'home.html', {'name': 'Juan Antonio'})
+    #return render (request, 'home.html')
+    #return render(request, 'home.html', {name: 'Juan Antonio'})
+    searchTerm = request.GET.get('searchMovie') # Captura lo que escribes en el input
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()   
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
